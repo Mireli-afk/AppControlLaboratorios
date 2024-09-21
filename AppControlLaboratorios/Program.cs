7d5f42351242967cce13cb0f7f5007f3d796735a
Mireli <gvasquezd22_2@unc.edu.pe>
2024-09-21 17:44:56 -0500

    EDIT: Vistas de login y barra de navegación

--- AppControlLaboratorios/Program.cs
+++ AppControlLaboratorios/Program.cs
@@ -39,6 +39,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Usuarios}/{action=Login}/{id?}");
 
 app.Run();
